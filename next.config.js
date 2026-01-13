572fbbdeca50c665d7a08a73087f64d8dbd11674
rodeok <nationx22@gmail.com>
2026-01-14 00:49:19 +0100

    g

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  webpack: (config) => {
+    config.externals.push({ encoding: 'encoding' });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
